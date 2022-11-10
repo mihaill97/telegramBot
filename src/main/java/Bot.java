bf8be889d3d683aa51c219d8d3a547ec292a2fca
Mihail97 <wowcirkle7991@gmail.com>
2022-11-10 14:14:24 +0200

    Initial commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,62 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+
+        }
+    }
+
+    public void sendMsg(Message message, String text){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+        execute(sendMessage);
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "UnicoreBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5618088727:AAE-Oansu6YszUU_2U8h97N_xvIa8kvy3iI";
+    }
+
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if(message != null && message.hasText()){
+            switch (message.getText()){
+                case "/help":
+                    sendMsg(message, "Чем могу помочь?");
+                    break;
+                case "/settings":
+                    sendMsg(message, "Что будем настраивать?");
+                    break;
+                default:
+                    sendMsg(message,"Доступные команды /help , /settings");
+            }
+        }
+
+    }
+}
